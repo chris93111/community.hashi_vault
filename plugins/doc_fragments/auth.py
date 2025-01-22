5625f39be23a6fc29337f20329aed4348d859375
chris93111 <christopheferreira@ymail.com>
2025-01-23 00:46:31 +0100

    Update auth.py

--- plugins/doc_fragments/auth.py
+++ plugins/doc_fragments/auth.py
@@ -135,6 +135,15 @@ class ModuleDocFragment(object):
         description: For C(cert) auth, path to the private key file to authenticate with, in PEM format.
         type: path
         version_added: 1.4.0
+      kubernetes_token:
+        description: The Kubernetes Token (JWT) to use for Kubernetes authentication to Vault.
+        type: str
+        version_added: 2.5.0
+      kubernetes_token_path:
+        description: If no kubernetes_token is specified, will try to read the token from this path.
+        default: '/var/run/secrets/kubernetes.io/serviceaccount/token'
+        type: str
+        version_added: 2.5.0
     '''
 
     PLUGINS = r'''
@@ -312,4 +321,17 @@ class ModuleDocFragment(object):
         ini:
           - section: hashi_vault_collection
             key: cert_auth_private_key
+      kubernetes_token:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_KUBERNETES_TOKEN
+        vars:
+          - name: ansible_hashi_vault_kubernetes_token
+      kubernetes_token_path:
+        env:
+          - name: ANSIBLE_HASHI_VAULT_KUBERNETES_TOKEN_PATH
+        ini:
+          - section: hashi_vault_collection
+            key: kubernetes_token_path
+        vars:
+          - name: ansible_hashi_vault_kubernetes_token_path
     '''
